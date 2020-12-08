c0c42d3cac20918ccb0e961327812b00ed9879bf
Viktor Szépe <viktor@szepe.net>
2020-12-08 13:04:48 +0100

    Make WP_Less_Is_More_Page_Walker shorter

--- classes/wp-less-is-more-page-walker.php
+++ classes/wp-less-is-more-page-walker.php
@@ -5,20 +5,12 @@
  *
  * @since WP Less is More 1.1.6
  */
-
-  class WP_Less_Is_More_Page_Walker extends Walker_Page {
+class WP_Less_Is_More_Page_Walker extends Walker_Page {
     public function start_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "\n$indent<ul class='dropdown-menu' role='menu'>\n";
+        $output .= sprintf( "\n%s<ul class='dropdown-menu' role='menu'>\n", str_repeat( "\t", $depth ) );
     }
 
     public function start_el( &$output, $page, $depth = 0, $args = array(), $current_page = 0 ) {
-        if ( $depth ) {
-            $indent = str_repeat( "\t", $depth );
-        } else {
-            $indent = '';
-        }
-
         $css_class = array( 'page_item', 'page-item-' . $page->ID );
 
         if ( isset( $args['pages_with_children'][ $page->ID ] ) ) {
@@ -62,37 +54,30 @@
         $args['link_before'] = empty( $args['link_before'] ) ? '' : $args['link_before'];
         $args['link_after'] = empty( $args['link_after'] ) ? '' : $args['link_after'];
 
+        $output .= str_repeat( "\t", $depth );
         /** This filter is documented in wp-includes/post-template.php */
-        if ( isset( $args['pages_with_children'][ $page->ID ] ) ) {
-            $output .= $indent . sprintf(
-                    '<li class="%s"><a href="%s" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">%s%s%s <span class="caret"></span></a>',
-                    $css_classes,
-                    get_permalink( $page->ID ),
-                    $args['link_before'],
-                    apply_filters( 'the_title', $page->post_title, $page->ID ),
-                    $args['link_after']
-                );
-        } else {
-            $output .= $indent . sprintf(
-                    '<li class="%s"><a href="%s">%s%s%s</a>',
-                    $css_classes,
-                    get_permalink( $page->ID ),
-                    $args['link_before'],
-                    apply_filters( 'the_title', $page->post_title, $page->ID ),
-                    $args['link_after']
-                );
-        }
+        $output .= isset( $args['pages_with_children'][ $page->ID ] )
+            ? sprintf(
+                '<li class="%s"><a href="%s" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">%s%s%s <span class="caret"></span></a>',
+                $css_classes,
+                get_permalink( $page->ID ),
+                $args['link_before'],
+                apply_filters( 'the_title', $page->post_title, $page->ID ),
+                $args['link_after']
+            )
+            : sprintf(
+                '<li class="%s"><a href="%s">%s%s%s</a>',
+                $css_classes,
+                get_permalink( $page->ID ),
+                $args['link_before'],
+                apply_filters( 'the_title', $page->post_title, $page->ID ),
+                $args['link_after']
+            );
 
 
         if ( ! empty( $args['show_date'] ) ) {
-            if ( 'modified' == $args['show_date'] ) {
-                $time = $page->post_modified;
-            } else {
-                $time = $page->post_date;
-            }
-
-            $date_format = empty( $args['date_format'] ) ? '' : $args['date_format'];
-            $output .= ' ' . mysql2date( $date_format, $time );
+            $time = 'modified' == $args['show_date'] ? $page->post_modified : $page->post_date;
+            $output .= ' ' . mysql2date( empty( $args['date_format'] ) ? '' : $args['date_format'], $time );
         }
     }
 }
