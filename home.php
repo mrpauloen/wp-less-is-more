7725751bec933d250a32a604f40d8200fd842027
Paweł <69369238+mrpauloen@users.noreply.github.com>
2022-10-22 21:17:57 +0200

    v1.1.8
    ## Released: Oktober 22, 2022
    
    ### Maintaining support
    
    * New Contributor on GitHub: @szepeviktor
    * Code reviewed and made some fixes
    * Added Starter Content

--- home.php
+++ home.php
@@ -25,18 +25,19 @@
 
 ?>
 		<p class="text-right">
+
       <a href="<?php the_permalink(); ?>" rel="<?php esc_attr_e( 'bookmark', 'wp-less-is-more' ); ?>" title="<?php esc_attr_e( 'Permanent Link to:', 'wp-less-is-more' ); ?>&nbsp;<?php the_title_attribute(); ?>" class="btn btn-default" role="button"> <?php _e( 'Read more...', 'wp-less-is-more' ); ?></a>
+
     </p>
 
-
 <!--
-
 <?php trackback_rdf(); ?>
-
 -->
+
 </article>
 
 <?php endwhile; else: ?>
+
 <p><?php _e( 'Sorry, no posts matched your criteria.', 'wp-less-is-more' ); ?></p>
 
 <?php endif;
@@ -44,12 +45,16 @@
 $curent_page_number = max( 1, get_query_var( 'paged', 1 ) );
 
     $args = array(
+
         'prev_next'          => true,
         'prev_text'          => '<span title="' . __( '&larr; Click to: Previous Page', 'wp-less-is-more' ) . '" class="badge bg-red">' . __( '&loarr; Previous', 'wp-less-is-more' ) . '</span>',
         'next_text'          => '<span title="' . __( 'Click to: Next Page &rarr;', 'wp-less-is-more' ) . '" class="badge bg-red">' . __( 'Next &roarr;', 'wp-less-is-more' ) . '</span>',
         'before_page_number' => '<span title="' . __( 'Another page number in pagination', 'wp-less-is-more' ) . '" class="badge bg-red">',
         'after_page_number'  => '</span>',
         'current' =>  $curent_page_number,
+
     );
+
 the_posts_pagination( $args );
+
 get_footer();
--- single.php
+++ single.php
@@ -3,10 +3,13 @@
 	if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <article id="site-content" class="text-break">
-<h3 class="text-center container single-entry-title entry-title"><?php wp_less_is_more__password_protcted(); ?><?php the_title(); ?></h3>
+
+<h3 class="container single-entry-title entry-title"><?php wp_less_is_more__password_protcted(); ?><?php the_title(); ?></h3>
+
 <hr/>
 
 <?php  the_post_thumbnail( 'post-thumbnail', array ( 'class' => 'img-responsive' )); ?>
+
 
 <div class="clearfix top"></div>
 
@@ -15,6 +18,7 @@
 	wp_link_pages(
 
 		$args = array(
+
 		'before'		=> '<p class="pager">' . __( 'Go to:', 'wp-less-is-more' ),
 		'after'			=> '</p>',
 		'link_before'	=> '<span class="btn btn-xs btn-danger">',
@@ -23,14 +27,13 @@
 		'pagelink'		=> _x( 'part: %', 'single post & page', 'wp-less-is-more' ),
 
 	));
+
 ?>
 
 <div class="clearfix"></div>
 
 <!--
-
 <?php trackback_rdf(); ?>
-
 -->
 
 <nav>
@@ -38,9 +41,11 @@
 <li class="previous"><?php 	previous_post_link( '%link', '<span aria-hidden="true">&larr;</span> '. __( 'Previous post', 'wp-less-is-more' ) ); ?></li>
 <li class="next"><?php 		next_post_link( '%link', __( 'Next post', 'wp-less-is-more' ).' <span aria-hidden="true">&rarr;</span>' ); ?></li>
 </ul>
+
 </nav>
 
 <hr/>
+
 <div class="entry-meta"><?php wp_less_is_more__entry_meta(); ?></div>
 
 <?php comments_template(); ?>
@@ -48,6 +53,9 @@
 </article>
 
 <?php endwhile; else: ?>
+
 <p><?php _e( 'Sorry, no posts matched your criteria.', 'wp-less-is-more' ); ?></p>
+
 <?php endif;
-get_footer(); ?>
+
+get_footer();
